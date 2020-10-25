b15b90cc1618530d13bfbf1330666d3b73762192
tmessall <tmessall17@gmail.com>
2020-10-24 21:16:13 -0600

    Dynamic port

--- db/server.js
+++ db/server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT =  3000;
+const PORT =  process.env.PORT || 3000;
 
 function displayNotes(res) {
     res.sendFile(path.join(__dirname,'','./../public/notes.html'));
