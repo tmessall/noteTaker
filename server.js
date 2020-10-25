d8923ca6903f451e10f7fe477aaf16235d470386
tmessall <tmessall17@gmail.com>
2020-10-24 21:28:58 -0600

    Changed something so files work

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 const PORT =  process.env.PORT || 3000;
 
 function displayNotes(res) {
-    res.json(path.join(__dirname,'','./../public/notes.html'));
+    res.sendFile(path.join(__dirname,'','./public/notes.html'));
 }
 
 function displayIndex(res) {
-    res.json(path.join(__dirname,'',"./../public/index.html"));
+    res.sendFile(path.join(__dirname,'',"./public/index.html"));
 }
 
 // Routes
