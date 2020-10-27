11c4cb96e1c84f245a1a9d2bd8a01ca2a7b32bc2
tmessall <tmessall17@gmail.com>
2020-10-26 18:38:35 -0600

    Added id to notes

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const PORT =  process.env.PORT || 3000;
 
 const notes = require("./db/db.json");
 
+for (let i = 0; i < notes.length; i++) {
+    notes[i].id = i;
+}
+
 function displayNotes(res) {
     res.sendFile(path.join(__dirname,'','./public/notes.html'));
 }
@@ -33,11 +37,12 @@ app.get("/api/notes", (req, res) => {
 app.post("/api/notes", (req, res) => {
     let notesArr = [...notes];
     notesArr.push(req.body);
+    notesArr[notes.length - 1].id = notesArr.length - 1;
     asyncWriteFile("./db/db.json", JSON.stringify(notesArr)).then(err => {
         if (err) console.log(err);
         res.end();
     })
-})
+});
 
 app.get("*", (req, res) => {
     displayIndex(res);
